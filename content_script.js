4104451ede3c1ccd911a1c5e696e24898d3e30d6
tusmanakhter <tusman96@hotmail.com>
2018-02-03 19:26:33 -0500

    Added carlos specific strings

--- content_script.js
+++ content_script.js
@@ -33,16 +33,28 @@ function buildWord(text) {
     for (var i = 0; i < splitted.length; i++) {
         var word = splitted[i];
         if (/[aeiou]/gi.test(word)) {
-            word = word.replace(/a/g, 'aa');
-            word = word.replace(/e/g, 'ee');
-            word = word.replace(/i/g, 'ii');
-            word = word.replace(/o/g, 'oo');
-            word = word.replace(/u/g, 'uu');
-            word = word.replace(/A/g, 'AA');
-            word = word.replace(/E/g, 'EE');
-            word = word.replace(/I/g, 'II');
-            word = word.replace(/O/g, 'OO');
-            word = word.replace(/U/g, 'UU');
+            word = word.replace(/^hey$/i, 'hey hey heyyyyy');
+            word = word.replace(/^wassup$/i, 'wassa wassa wassa wassuuup');
+            word = word.replace(/^sup$/i, 'wassa wassa wassa wassuuup');
+            word = word.replace(/^whats up$/i, 'wassa wassa wassa wassuuup');
+            word = word.replace(/^what's up$/i, 'wassa wassa wassa wassuuup');
+            word = word.replace(/connect/i, 'conneeeeect');
+            word = word.replace(/^love$/i, 'looooove');
+            word = word.replace(/^amazing$/i, 'ammmaaasing');
+            word = word.replace(/^no$/i, 'mmm mmm nonono');
+            word = word.replace(/^waves$/i, 'waaaaaves');
+            word = word.replace(/^go$/i, 'gOoOoOo');
+            word = word.replace(/^25610$/i, '140000');
+            word = word.replace(/^independently$/i, 'independently financially independently');
+            word = word.replace(/^woah$/i, 'woooaaah');
+            word = word.replace(/^yea$/i, 'yeyeyeyeyea');
+            word = word.replace(/^Vietnam$/i, 'Hey Hey Vietnam');
+            word = word.replace(/^good$/i, 'goooood');
+            word = word.replace(/^seed$/i, 'seeeeed');            
+            word = word.replace(/^world$/i, 'wooooorld');
+            word = word.replace(/^connect$/i, 'conneeeeect');
+            word = word.replace(/^love$/i, 'looooove');
+            word = word.replace(/\?$/, '? (What am I gonna do?)');
             splitted[i] = word;
         }
     }
